e8a66f5b0c3ac1dcf146f730981757a419c941ed
RiyaE3 <riya@e3electric.ai>
2025-05-09 10:22:55 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -12,22 +12,22 @@ st.title('Predict Temperature Sensor 1')
 
 # Number inputs with step=0.0001 for 4 decimal places
 soc = st.number_input('SOC', value=72.0000, step=0.0001, format="%.4f")
-voltage = st.number_input('Voltage', value=53.0000, step=0.0001, format="%.4f")
-current = st.number_input('Current', value=0.0000, step=0.0001, format="%.4f")
+voltage = st.number_input('Voltage', value=51.4000, step=0.0001, format="%.4f")
+current = st.number_input('Current', value=29.1000, step=0.0001, format="%.4f")
 
 # Default cell voltages
-default_cell_voltages = [3.322, 3.322, 3.322, 3.322, 3.322, 3.319, 3.325, 3.321, 
-                        3.322, 3.322, 3.322, 3.322, 3.322, 3.322, 3.322, 3.322]
+default_cell_voltages = [3.269, 3.266, 3.267, 3.266, 3.25, 3.246, 3.252, 3.243,
+                        3.232, 3.25, 3.25, 3.25, 3.25, 3.247, 3.247, 3.263]
 
 cell_voltages = []
 for i in range(1, 17):
     cell_volt = st.number_input(f'Cell Volt {i}', value=default_cell_voltages[i-1], step=0.0001, format="%.4f")
     cell_voltages.append(cell_volt)
 
-temp_sensor_2 = st.number_input('Temperature Sensor 2', value=29.0000, step=0.0001, format="%.4f")
-temp_sensor_3 = st.number_input('Temperature Sensor 3', value=29.0000, step=0.0001, format="%.4f")
-temp_sensor_4 = st.number_input('Temperature Sensor 4', value=29.0000, step=0.0001, format="%.4f")
-temp_sensor_5 = st.number_input('Temperature Sensor 5', value=29.0000, step=0.0001, format="%.4f")
+temp_sensor_2 = st.number_input('Temperature Sensor 2', value=33.0000, step=0.0001, format="%.4f")
+temp_sensor_3 = st.number_input('Temperature Sensor 3', value=33.0000, step=0.0001, format="%.4f")
+temp_sensor_4 = st.number_input('Temperature Sensor 4', value=33.0000, step=0.0001, format="%.4f")
+temp_sensor_5 = st.number_input('Temperature Sensor 5', value=33.0000, step=0.0001, format="%.4f")
 
 if st.button('Predict Temperature Sensor 1'):
     input_data = np.array(
